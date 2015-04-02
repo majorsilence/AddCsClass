9f7a6580056634b12d67403d907f596d0feee7f9
Peter Gill <peter.gill@townsuite.com>
2015-04-02 16:44:47 -0230

    Simple tool to add .cs files to csproj projects

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AddCsClass
-A a new c# file to a c# project file
+Add a new c# file to a c# project file from commandline.
+
+There is probably a much better way to do this and I would be grateful if someone pointed it out.  Mabye [edge](https://github.com/tjanczuk/edge) but that seems like a lot for just this one requirement.
+
+# Example
+
+```bash
+AddCsClass.exe "path to project file.csproj" "path to new .cs file"
+```
---
+++ src/AddCsClass/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AddCsClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Count() != 2)
+            {
+                HelpMenu();
+                return;
+            }
+
+
+            var projectFile = args[0];
+            var newSourceFile = args[1];
+
+
+            AddFileProject(newSourceFile, projectFile);
+
+        }
+
+        private static void HelpMenu()
+        {
+            System.Console.WriteLine("This program will add a .cs file to an existing c# project.");
+            System.Console.WriteLine("");
+            System.Console.WriteLine("AddCsClass.exe \"path to project file.csproj\" \"path to new .cs file\"");
+
+        }
+
+        private static XElement AddProjectContent(string pathToAdd, XDocument doc)
+        {
+            XNamespace rootNamespace = doc.Root.Name.NamespaceName;
+            var xelem = new XElement(rootNamespace + "Compile");
+            xelem.Add(new XAttribute("Include", pathToAdd));
+            return xelem;
+        }
+
+        private static void AddFileProject(string pathToAdd, string projectFilePath)
+        {
+
+            var projectFileXml = XDocument.Load(projectFilePath);
+            var itemGroup =
+            projectFileXml.Nodes()
+                               .OfType<XElement>()
+                               .DescendantNodes()
+                               .OfType<XElement>().First(xy => xy.Name.LocalName == "ItemGroup");
+            var xelem = AddProjectContent(pathToAdd, projectFileXml);
+            itemGroup.Add(xelem);
+            projectFileXml.Save(projectFilePath);
+        }
+
+    }
+}
